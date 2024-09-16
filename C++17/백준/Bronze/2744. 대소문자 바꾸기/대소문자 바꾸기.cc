823574021a942b99f3b72925761a60b01022b385
nete32 <netedevofficial@gmail.com>
2024-09-16 21:05:42 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s; cin >> s;
+	for (int i = 0; i < s.length(); i++) {
+		if (isupper(s[i]))
+			cout << (char)(s[i] - 'A' + 'a');
+		else
+			cout << (char)(s[i] - 'a' + 'A');
+	}
+
+	return 0;
+}
