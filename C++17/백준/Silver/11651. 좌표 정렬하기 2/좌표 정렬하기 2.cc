561463e710a151ee6168fe9da7f314e7cc2dc17d
nete32 <netedevofficial@gmail.com>
2024-09-17 17:42:15 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Point {
+	int x, y;
+};
+
+bool cmp(Point& a, Point& b)
+{
+	if (a.y == b.y)
+		return a.x < b.x;
+	return a.y < b.y;
+}
+
+Point p[100001];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> p[i].x >> p[i].y;
+
+	sort(p, p + N, cmp);
+	
+	for (int i = 0; i < N; i++) {
+		cout << p[i].x << " " << p[i].y << "\n";
+	}
+
+	return 0;
+}
