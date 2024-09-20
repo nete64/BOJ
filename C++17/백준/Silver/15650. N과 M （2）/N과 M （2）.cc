4d86767bac1ba9f68c498c6d89e55f566e80fa73
nete32 <netedevofficial@gmail.com>
2024-09-20 13:31:37 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[9];
+void bt(int depth, int curr)
+{
+	if (depth == M) {
+		for (int i = 0; i < M; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = curr; i < N; i++) {
+		arr[depth] = i + 1;
+		bt(depth + 1, i + 1);
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+	bt(0, 0);
+	return 0;
+}
