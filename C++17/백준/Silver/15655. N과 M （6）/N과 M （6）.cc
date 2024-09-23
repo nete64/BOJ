434e44fb25dc1b99d91f6bfd8d17cbded9251894
nete32 <netedevofficial@gmail.com>
2024-09-23 22:17:48 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[10], sorted[10];
+bool visited[10002];
+void bt(int depth, int curr)
+{
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = curr; i < N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			arr[depth] = sorted[i];
+			bt(depth + 1, i + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		cin >> sorted[i];
+	sort(sorted, sorted + N);
+	bt(0, 0);
+
+	return 0;
+}
