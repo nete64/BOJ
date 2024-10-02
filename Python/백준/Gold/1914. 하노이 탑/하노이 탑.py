47066dbe7b96afb3d39a7d9cc7792d739658e783
nete32 <netedevofficial@gmail.com>
2024-10-02 19:36:10 +0900

    [Gold V] Title: 하노이 탑, Time: 744 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,13 @@
+def hanoi(a, b, n):
+  if n == 1:
+    print(a, b)
+    return
+
+  hanoi(a, 6-a-b, n-1)
+  print(a, b)
+  hanoi(6-a-b, b, n-1)
+
+n = int(input())
+print(2**n-1)
+if n <= 20:
+  hanoi(1, 3, n)
