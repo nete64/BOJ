5875d3297ebd93eacb9f706b28d9345048e7c6c3
nete32 <netedevofficial@gmail.com>
2024-10-03 23:24:59 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1002], dp[1002];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+		dp[i] = arr[i];
+	}
+
+	dp[1] = arr[1];
+	int ans = dp[1];
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[i], dp[j] + arr[i]);
+				ans = max(ans, dp[i]);
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
