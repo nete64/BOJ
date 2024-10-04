c19568bfb0495e92da8727b7cac467b7de9636e1
nete32 <netedevofficial@gmail.com>
2024-10-04 14:31:39 +0900

    [Gold V] Title: 행복 유치원, Time: 164 ms, Memory: 4364 KB -BaekjoonHub

---
+++ C++17/백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[300002], dist[300002];
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N - 1; i++)
+		dist[i] = arr[i + 1] - arr[i];
+
+	sort(dist, dist + N - 1);
+
+	int ans = 0;
+	for (int i = 0; i < N - K; i++)
+		ans += dist[i];
+
+	cout << ans;
+	return 0;
+}
