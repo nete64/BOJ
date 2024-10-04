74bc7f970518ef39621b0101bf7c886059eec573
nete32 <netedevofficial@gmail.com>
2024-10-04 14:21:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 32 ms, Memory: 4720 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+const int MAX_V = 1002;
+const int INF = 1'000'000'000;
+using P = pair<int, int>;
+
+int dist[MAX_V];
+bool visited[MAX_V];
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	vector<P> adj[MAX_V];
+	int N, M; cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int u, v, w; cin >> u >> v >> w;
+		adj[u].push_back(P(v, w));
+	}
+
+	int S, E; cin >> S >> E;
+
+	fill(dist, dist + MAX_V, INF);
+
+	priority_queue<P, vector<P>, greater<P> > pq;
+
+	dist[S] = 0;
+	pq.push(P(0, S));
+	while (!pq.empty()) {
+		int curr;
+		do {
+			curr = pq.top().second;
+			pq.pop();
+		} while (!pq.empty() && visited[curr]);
+
+		if (visited[curr])
+			break;
+		
+		visited[curr] = true;
+		for (auto &p : adj[curr]) {
+			int next = p.first;
+			int d = p.second;
+
+			if (dist[next] > dist[curr] + d) {
+				dist[next] = dist[curr] + d;
+				pq.push(P(dist[next], next));
+			}
+		}
+	}
+
+	cout << dist[E];
+}
