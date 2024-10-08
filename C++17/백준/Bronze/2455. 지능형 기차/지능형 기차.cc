6a1ae3cf0ee0a263b863f83f8470be7a3cbe1d60
nete32 <netedevofficial@gmail.com>
2024-10-08 23:03:24 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int cnt = 0, ans = 0;
+	for (int i = 0; i < 4; i++) {
+		int a, b; cin >> a >> b;
+		cnt += b - a;
+		ans = max(ans, cnt);
+	}
+	cout << ans;
+	return 0;
+}
