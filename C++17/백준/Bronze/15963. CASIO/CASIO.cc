58928cdecb7fbc08871b3033a51a76cafbf967ac
nete32 <netedevofficial@gmail.com>
2024-10-10 23:18:40 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	long long a, b; cin >> a >> b;
+	if (a == b) cout << "1";
+	else cout << "0";
+}
