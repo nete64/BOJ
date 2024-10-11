60c8316f0893ee62574a465a576d0a8c932fed58
nete32 <netedevofficial@gmail.com>
2024-10-11 09:37:33 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    unsigned long long int N, M; cin >> N >> M;
+    cout << N * M / 2;
+
+    return 0;
+}
