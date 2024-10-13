a059f54c408570dc097ef12acef7e0f66fedadfb
nete32 <netedevofficial@gmail.com>
2024-10-14 08:55:46 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|";
+
+	return 0;
+}
