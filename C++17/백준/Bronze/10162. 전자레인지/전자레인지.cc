3415d05e2451bf5fc1ccfcaeb1fb8caaed007c92
nete32 <netedevofficial@gmail.com>
2024-10-19 01:17:21 +0900

    [Bronze III] Title: 전자레인지, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ C++17/백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T; cin >> T;
+	if (T % 10 != 0)
+		cout << "-1";
+	else
+		cout << T / 300 << " " << T / 60 << " " << (T % 60) / 10;
+}
