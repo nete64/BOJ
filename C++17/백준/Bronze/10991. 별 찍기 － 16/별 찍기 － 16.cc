bbf9ec3aea8f95bbde240467c5fb207d0890b21d
nete32 <netedevofficial@gmail.com>
2024-10-19 14:45:25 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[3];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N - i - 1; j++)
+            cout << " ";
+        for (int j = 0; j < 2 * i + 1; j++) {
+            if (j % 2 == 0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+	return 0;
+}
