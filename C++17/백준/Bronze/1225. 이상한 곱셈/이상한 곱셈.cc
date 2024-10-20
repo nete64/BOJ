23b1fe8ba64cc45646881cdb8243f36ca5bc148d
nete32 <netedevofficial@gmail.com>
2024-10-21 00:54:26 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 68 ms, Memory: 2168 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string A, B; cin >> A >> B;
+	if (A.size() < B.size())
+		swap(A, B);
+
+	long long ans = 0;
+	for (auto& b : B) {
+		for (auto& a : A) {
+			ans += (a - '0') * (b - '0');
+		}
+	}
+	cout << ans;
+	return 0;
+}
