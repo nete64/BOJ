4adb7789be0099f2bce6b37a730af13647d790ce
nete32 <netedevofficial@gmail.com>
2024-10-23 08:56:39 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M; cin >> N >> M;
+    cout << N * M - 1;
+    return 0;
+}
