07c69176262cea44dde1a7b0e7d75cb31e8a241e
nete32 <netedevofficial@gmail.com>
2024-10-22 09:16:29 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
+++ C++17/백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
@@ -2,13 +2,13 @@
 #include <algorithm>
 using namespace std;
 
-int arr[3];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> arr[0] >> arr[1] >> arr[2];
-    cout << max(arr[1] - arr[0], arr[2] - arr[1]) - 1;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << max(b - a, c - b) - 1;
     return 0;
 }
