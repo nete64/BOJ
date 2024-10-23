057e7ba3c848041eb2f5ccff85702ff0ce1dfd05
nete32 <netedevofficial@gmail.com>
2024-10-23 12:32:40 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int i = 1;
+    while (true) {
+        int N; cin >> N;
+        if (N == 0)
+            break;
+
+        while (N--) {
+            int X; cin >> X;
+        }
+        cout << "Case " << i << ": Sorting... done!\n";
+        i++;
+    }
+    return 0;
+}
