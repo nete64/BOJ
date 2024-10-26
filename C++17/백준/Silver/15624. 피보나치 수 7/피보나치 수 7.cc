3cd01496138c5f697ed7b3aee37eab9dab3bf8e5
nete32 <netedevofficial@gmail.com>
2024-10-26 19:17:09 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 9832 KB, Score: 201 point -BaekjoonHub

---
+++ C++17/백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+ll dp[1000001];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    dp[1] = 0;
+    dp[2] = 1;
+    for (int i = 3; i <= n + 1; i++)
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
+    cout << dp[n+1];
+    return 0;
+}
