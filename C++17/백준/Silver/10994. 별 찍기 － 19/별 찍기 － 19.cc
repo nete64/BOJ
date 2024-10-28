82bd624a9fe82de34637eed242b799253cd1a191
nete32 <netedevofficial@gmail.com>
2024-10-28 14:04:24 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 4 ms, Memory: 99656 KB -BaekjoonHub

---
+++ C++17/백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+char c[9999][9999];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+    int s = 4 * (N - 1) + 1;
+    int a = 0, b = s;
+    while (a <= b) {
+        for (int i = a; i < b; i++)
+            c[a][i] = c[b - 1][i] = c[i][a] = c[i][b - 1] = '*';
+        a += 2;
+        b -= 2;
+    }
+
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            if (c[i][j] == '*')
+                cout << c[i][j];
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
