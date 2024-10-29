8ab41d52e9cf1b577548dcfaab4d4bbe4fab7640
nete32 <netedevofficial@gmail.com>
2024-10-29 09:21:55 +0900

    [Silver IV] Title: 필터, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1895. 필터/필터.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arr[42][42];
+#include <vector>
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int R, C; cin >> R >> C;
+    for (int i = 0; i < R; i++)
+        for (int j = 0; j < C; j++)
+            cin >> arr[i][j];
+    int T; cin >> T;
+
+    vector<int> f;
+    for (int m = 0; m + 2 < R; m++) {
+        for (int i = 0; i + 2 < C; i++) {
+            vector<int> v;
+            for (int j = 0; j < 3; j++)
+                for (int k = 0; k < 3; k++)
+                    v.push_back(arr[m + j][i + k]);
+            sort(v.begin(), v.end());
+            f.push_back(v[v.size() / 2]);
+        }
+    }
+
+    int ans = 0;
+    for (auto &e : f)
+        if (e >= T)
+            ans++;
+    cout << ans;
+	return 0;
+}
