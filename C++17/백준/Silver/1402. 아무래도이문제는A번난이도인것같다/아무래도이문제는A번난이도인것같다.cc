76f03ba5eb96fe24ca087d2ecd25291781a2bf9a
nete32 <netedevofficial@gmail.com>
2024-10-29 16:06:48 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    int T; cin >> T;
+    while (T--) {
+        int a, b; cin >> a >> b;
+        cout << "yes\n";
+    }
+	return 0;
+}
