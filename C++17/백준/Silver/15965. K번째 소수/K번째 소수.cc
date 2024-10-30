6158ddbddee7927daf95ddbf57977d4b88b8331c
nete32 <netedevofficial@gmail.com>
2024-10-30 16:42:42 +0900

    [Silver II] Title: K번째 소수, Time: 1540 ms, Memory: 148956 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Silver/15965. K번째 소수/K번째 소수.cc
+++ C++17/백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -3,26 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+const int MAX = 1e8;
 
 vector<int> v;
-bool isPrime[500002];
+bool isPrime[MAX];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
     
-    fill(isPrime, isPrime + 500002, true);
+    fill(isPrime, isPrime + MAX, true);
 
     isPrime[1] = false;
-    for (int i = 2; i <= sqrt(500000); i++) {
+    for (int i = 2; i <= sqrt(MAX); i++) {
         if (!isPrime[i]) continue;
 
-        for (int j = i * i; j <= 500000; j += i) {
+        for (int j = i * i; j <= MAX - 1; j += i) {
             isPrime[j] = false;
         }
     }
 
-    for (int i = 2; i <= 500000; i++)
+    for (int i = 2; i <= MAX - 1; i++)
         if (isPrime[i])
             v.push_back(i);
 
