966c096f131933630590a09b17e7baedb394e830
nete32 <netedevofficial@gmail.com>
2024-11-04 15:10:54 +0900

    [Silver IV] Title: 화살표 그리기, Time: 0 ms, Memory: 2048 KB, Score: 89 point -BaekjoonHub

---
+++ C++17/백준/Silver/15970. 화살표 그리기/화살표 그리기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+vector<int> v[1001];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    
+	int N; cin >> N;
+	for (int i = 1; i <= N; i++) {
+		int x, y; cin >> x >> y;
+		v[y].push_back(x);
+	}
+	for (int i = 1; i <= N; i++)
+		sort(v[i].begin(), v[i].end());
+	
+	int ans = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			if (j == 0) ans += abs(v[i][j] - v[i][j + 1]);
+			else if (j == v[i].size() - 1) ans += abs(v[i][j] - v[i][j - 1]);
+			else ans += min(abs(v[i][j] - v[i][j + 1]), abs(v[i][j] - v[i][j - 1]));
+		}
+	}
+	cout << ans;
+	return 0;
+}
