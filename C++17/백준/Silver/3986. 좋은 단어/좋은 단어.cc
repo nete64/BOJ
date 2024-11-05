7d110a4aada8eccc7a7a023e4dc5dd418db96df3
nete32 <netedevofficial@gmail.com>
2024-11-05 14:07:18 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- C++17/백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ C++17/백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -13,30 +13,14 @@ int main()
     int N; cin >> N;
     while (N--) {
         stack<char> st;
-        bool chk = true;
-        int A = 0, B = 0;
-
         string s; cin >> s;
         for (auto& c : s) {
-            if (!st.empty()) {
-                if (st.top() == c) {
-                    if (c == 'A') A--;
-                    else B--;
-                    st.pop();
-                } else {
-                    if (A > 1 || B > 1) {
-                        chk = false;
-                        break;
-                    } else {
-                        st.push(c);
-                    }
-                }
-            } else {
+            if (!st.empty() && st.top() == c)
+                st.pop();
+            else
                 st.push(c);
-            }
         }
-
-        if (chk && st.empty()) ans++;
+        if (st.empty()) ans++;
     }
     cout << ans;
     return 0;
