0026c18162f8c51e005704c06c0fb2c9894a1658
nete32 <netedevofficial@gmail.com>
2024-11-16 20:09:31 +0900

    [Gold IV] Title: Fence Repair, Time: 4 ms, Memory: 2532 KB -BaekjoonHub

---
+++ C++17/백준/Gold/6195. Fence Repair/Fence Repair.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <functional>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	priority_queue<ll, vector<ll>, greater<ll> > pq;
+
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+		ll X; cin >> X;
+		pq.push(X);
+	}
+
+	ll ans = 0;
+	while (pq.size() > 1) {
+		ll K = pq.top(); pq.pop();
+		K += pq.top(); pq.pop();
+		ans += K;
+		pq.push(K);
+	}
+	cout << ans << "\n";
+
+	return 0;
+}
