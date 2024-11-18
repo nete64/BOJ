ac607cd0a2dd5ce2ea1ab1d78b3485b7dd2ec79f
nete32 <netedevofficial@gmail.com>
2024-11-18 19:18:56 +0900

    [Gold V] Title: 강의실, Time: 44 ms, Memory: 3700 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+struct P {
+	int s, t;
+};
+bool cmp(P& a, P& b)
+{
+	return a.s < b.s;
+}
+P p[100002];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+        int X;
+		cin >> X >> p[i].s >> p[i].t;
+    }
+
+	sort(p, p + N, cmp);
+
+	priority_queue<int, vector<int>, greater<int> > wait;
+	wait.push(p[0].t);
+	for (int i = 1; i < N; i++) {
+		if (!wait.empty() && p[i].s >= wait.top()) {
+			wait.pop();
+			wait.push(p[i].t);
+		}
+		else {
+			wait.push(p[i].t);
+		}
+	}
+	cout << wait.size();
+	
+	return 0;
+}
