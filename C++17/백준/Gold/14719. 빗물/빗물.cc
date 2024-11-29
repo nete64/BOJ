3f0abd846e119ec7ac4a2d9f12a65cb81f3d7ad6
nete32 <netedevofficial@gmail.com>
2024-11-29 14:57:27 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 3016 KB -BaekjoonHub

---
+++ C++17/백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int arr[505][505], ans;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int H, W; cin >> H >> W;
+    for (int i = 1; i <= W; i++) {
+        int X; cin >> X;
+        for (int j = 0; j < X; j++) {
+            arr[H - j][i] = 1;
+        }
+    }
+
+    for (int i = 1; i <= H; i++) {
+        int s = 1, e = 1;
+        for (int j = 1; j <= W; j++) {
+            if (j + 1 > W) continue;
+            if (arr[H - i + 1][j] == 0) {
+                s = j;
+                e = s + 1;
+                while (e <= W && arr[H - i + 1][e] == 0) {
+                    e++;
+                }
+                j = e;
+            }
+            if (e > W) continue;
+            if (s == 1) continue;
+            for (int k = s; k < e; k++) {
+                arr[H - i + 1][k] = 2;
+                // cout << i << " & " << "s e " << s << " " << e << "\n";
+                // ans++;
+            }
+        }
+    }
+
+    for (int i = 0; i <= H + 1; i++) {
+        for (int j = 0; j <= W + 1; j++) {
+            if (arr[i][j] == 2)
+                ans++;
+            //cout << arr[i][j] << " ";
+        }
+        //cout << "\n";
+    }
+    cout << ans;
+    return 0;
+}
