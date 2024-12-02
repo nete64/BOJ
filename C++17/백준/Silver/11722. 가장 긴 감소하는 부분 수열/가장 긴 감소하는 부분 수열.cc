6b71c3d41d19d78e2d875c22958fb1e18f75a79d
nete32 <netedevofficial@gmail.com>
2024-12-02 13:05:25 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1002], dp[1002];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] < arr[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        ans = max(ans, dp[i] + 1);
+    cout << ans;
+    return 0;
+}
