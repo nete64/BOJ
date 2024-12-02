d44e2c6ec3b86074f91174e8b333764adfb3958e
nete32 <netedevofficial@gmail.com>
2024-12-02 15:48:36 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int T[20], P[20], dp[20];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N; cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> T[i] >> P[i];
+    
+    for (int i = 0; i < N + 1; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (i >= j + T[j]) {
+                dp[i] = max(dp[i], dp[j] + P[j]);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < N + 1; i++)
+        ans = max(ans, dp[i]);
+    cout << ans;
+    return 0;
+}
