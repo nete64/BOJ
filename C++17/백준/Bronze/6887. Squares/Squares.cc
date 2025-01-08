508a950e82be10ca7048b1cc65ae488fe39b3ab2
nete64 <netedevofficial@gmail.com>
2025-01-08 13:07:07 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int N; cin >> N;
+	cout << "The largest square has side length " << (int)sqrt(N) << ".";
+	return 0;
+}
