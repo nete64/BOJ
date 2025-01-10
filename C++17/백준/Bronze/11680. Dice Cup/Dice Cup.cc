fdd7668cf39d6ef83f19731a68470d868af12efc
nete64 <netedevofficial@gmail.com>
2025-01-10 17:31:36 +0900

    [Bronze II] Title: Dice Cup, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/11680. Dice Cup/Dice Cup.cc
+++ C++17/백준/Bronze/11680. Dice Cup/Dice Cup.cc
@@ -9,20 +9,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    int N, M; cin >> N >> M;
+    int N, M, mx = -1e9; cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
             w[i + j]++;
         }
     }
 
-    int mx = -1e9, mxi = 0;
-    for (int i = 1; i < 50; i++) {
-        if (mx < w[i]) {
-            mx = w[i];
-            mxi = i;
-        }
-    }
+    for (int i = 1; i < 50; i++)
+        mx = max(mx, w[i]);
+
     for (int i = 1; i < 50; i++)
         if (mx == w[i])
             cout << i << "\n";
