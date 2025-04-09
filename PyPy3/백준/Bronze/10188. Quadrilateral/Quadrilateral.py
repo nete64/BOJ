c58442a77fe0a00bf2358a720668b96cc4f3b30c
nete64 <netedevofficial@gmail.com>
2025-04-09 12:49:56 +0900

    [Bronze IV] Title: Quadrilateral, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/10188. Quadrilateral/Quadrilateral.py
@@ -0,0 +1,8 @@
+T=int(input())
+for k in range(T):
+    w,h=map(int,input().split())
+    for i in range(h):
+        for j in range(w):
+            print("X",end='')
+        print("")
+    print("")
