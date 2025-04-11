11e05f3a308f20a5dd986a10decbdaa0a15cf5d9
nete64 <netedevofficial@gmail.com>
2025-04-11 15:14:58 +0900

    [Bronze IV] Title: Pokemon Buddy, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.py
+++ PyPy3/백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.py
@@ -1,9 +1,5 @@
 T=int(input())
 for i in range(T):
     G,C,E=map(int,input().split())
-    if C>=E:
-        print(0)
-        continue
-    if G==1:print(E-C)
-    elif G==2:print((E-C)*3)
-    elif G==3:print((E-C)*5)
+    if C>=E:print(0)
+    else:print((E-C)*(2*G-1))
