b5f581922cc4de4d11b542dfbccd125db44eb908
nete64 <netedevofficial@gmail.com>
2025-04-15 23:10:36 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
+++ PyPy3/백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
@@ -1,11 +1,5 @@
 m={0:'N',1:'E',2:'S',3:'W'}
 x=0
 for i in range(10):
-    t=int(input())
-    if t==1:x+=1
-    elif t==2:x+=2
-    else:x-=1
-    
-    if x>3:x-=4
-    elif x<0:x+=4
-print(m[x])
+    x+=int(input())
+print(m[x%4])
