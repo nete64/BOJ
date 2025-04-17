be44ecb1866e3433d8ca483fbbc252b18d7593d8
nete64 <netedevofficial@gmail.com>
2025-04-17 12:43:10 +0900

    [Bronze IV] Title: Yalda, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,14 @@
+b=int(input())
+x=int(input())
+if x<=b:
+    print("Watermelon")
+    exit()
+y=int(input())
+if y<=b:
+    print("Pomegranates")
+    exit()
+z=int(input())
+if z<=b:
+    print("Nuts")
+    exit()
+print("Nothing")
