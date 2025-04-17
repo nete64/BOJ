7ae06f0266fb091e7c8524edd2863b665163cf30
nete64 <netedevofficial@gmail.com>
2025-04-17 10:48:51 +0900

    [Bronze IV] Title: Generalized FizzBuzz, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/32801. Generalized FizzBuzz/Generalized FizzBuzz.py
@@ -0,0 +1,10 @@
+n,a,b=map(int,input().split())
+x=y=z=0
+for i in range(1,n+1):
+    if i%a==0 and i%b==0:
+        z+=1
+    elif i%b==0:
+        y+=1
+    elif i%a==0:
+        x+=1
+print(x,y,z)
