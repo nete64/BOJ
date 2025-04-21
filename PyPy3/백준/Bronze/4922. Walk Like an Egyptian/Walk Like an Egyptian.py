5ec7fc5ba7195c691b2b14395da75745cd16f9f9
nete64 <netedevofficial@gmail.com>
2025-04-21 16:15:04 +0900

    [Bronze III] Title: Walk Like an Egyptian, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- PyPy3/백준/Bronze/4922. Walk Like an Egyptian/Walk Like an Egyptian.py
+++ PyPy3/백준/Bronze/4922. Walk Like an Egyptian/Walk Like an Egyptian.py
@@ -2,10 +2,4 @@ while True:
     N=int(input())
     if N==0:
         break
-    
-    c=1
-    s=2
-    while c<=(N-1)**2:
-        c+=s
-        s+=2
-    print(f"{N} => {c}")
+    print(f"{N} => {N*(N-1)+1}")
