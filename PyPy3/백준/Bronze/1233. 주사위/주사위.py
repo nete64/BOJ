540c93ef953417b391999eb20deb3794ae5f8238
nete64 <netedevofficial@gmail.com>
2025-04-25 11:51:48 +0900

    [Bronze II] Title: 주사위, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- PyPy3/백준/Bronze/1233. 주사위/주사위.py
+++ PyPy3/백준/Bronze/1233. 주사위/주사위.py
@@ -4,8 +4,4 @@ for i in range(1,s[0]+1):
     for j in range(1,s[1]+1):
         for k in range(1,s[2]+1):
             v[i+j+k]+=1
-mx=max(v)
-for i in range(len(v)):
-    if mx==v[i]:
-        print(i)
-        break
+print(v.index(max(v)))
