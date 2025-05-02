07d02f18f9320644a749a39c4494c6e8dc1a31d6
nete64 <netedevofficial@gmail.com>
2025-05-02 15:57:50 +0900

    [Silver V] Title: K-세준수, Time: 216 ms, Memory: 110736 KB -BaekjoonHub

--- PyPy3/백준/Silver/1418. K－세준수/K－세준수.py
+++ PyPy3/백준/Silver/1418. K－세준수/K－세준수.py
@@ -2,14 +2,11 @@ N=int(input())
 K=int(input())
 c=0
 for i in range(1,N+1):
-    m=i
-    r=1
-    for j in range(2, int(i**0.5)+2):
+    r=m=i
+    for j in range(2, K+1):
         while m%j==0:
-            r=j
             m//=j
-        if m>1:
-            r=m
-    if r<=K:
+            r=j  
+    if m>1:
         c+=1
-print(c)
+print(N-c)
