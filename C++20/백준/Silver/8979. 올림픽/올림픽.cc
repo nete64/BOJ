aaa76f1346b52bdfef0a353dac354ab1acc748a9
nete64 <netedevofficial@gmail.com>
2025-05-07 14:47:40 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2040 KB, Score: 8 point -BaekjoonHub

---
+++ C++20/백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct C {
+    int i,a,b,c;
+};
+bool cmp(C& A, C& B)
+{
+    if (A.a == B.a && A.b == B.b)
+        return A.c > B.c;
+    else if (A.a == B.a)
+        return A.b > B.b;
+    return A.a > B.a;
+}
+C arr[1002];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N,K; cin >> N >> K;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i].i >> arr[i].a >> arr[i].b >> arr[i].c;
+
+    sort(arr, arr + N, cmp);
+
+    int r = 1;
+    int x = arr[0].a, y = arr[0].b, z = arr[0].c;
+    for (int i = 1; i < N; i++) {
+        if (!(arr[i].a == x && arr[i].b == y && arr[i].c == z)) {
+            r++;
+            x = arr[i].a;
+            y = arr[i].b;
+            z = arr[i].c;
+        }
+        if (arr[i].i == K)
+            break;
+    }
+    cout << r;
+    return 0;
+}
